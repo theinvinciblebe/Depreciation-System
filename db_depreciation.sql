262545a2ceb1c3defdf4a082b4298326466902b4
Theng Sothea <151487233+theinvinciblebe@users.noreply.github.com>
2026-06-25 18:12:21 +0700

    Add files via upload

--- db_depreciation.sql
+++ db_depreciation.sql
@@ -1,17 +1,17 @@
 /*
- Navicat Premium Data Transfer
+ Navicat Premium Dump SQL
 
  Source Server         : MySQL
  Source Server Type    : MySQL
- Source Server Version : 100428
+ Source Server Version : 80403 (8.4.3)
  Source Host           : localhost:3306
  Source Schema         : db_depreciation
 
  Target Server Type    : MySQL
- Target Server Version : 100428
+ Target Server Version : 80403 (8.4.3)
  File Encoding         : 65001
 
- Date: 31/12/2023 10:45:48
+ Date: 25/06/2026 18:02:30
 */
 
 SET NAMES utf8mb4;
@@ -23,19 +23,19 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `tblcustomer`;
 CREATE TABLE `tblcustomer`  (
   `cusid` bigint NOT NULL,
-  `cusname` longtext CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
-  `custel` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
-  `IDCard` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
-  `cusAddress` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
-  `Photo` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
-  `ProductName` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
+  `cusname` longtext CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL,
+  `custel` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL,
+  `IDCard` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL,
+  `cusAddress` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL,
+  `Photo` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL,
+  `ProductName` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL,
   `ProductPrice` double NULL DEFAULT NULL,
   `Interest` double NULL DEFAULT NULL,
   `Duration` int NULL DEFAULT NULL,
   `Create_Date` datetime NULL DEFAULT NULL,
   `Userid` bigint NULL DEFAULT NULL,
   PRIMARY KEY (`cusid`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of tblcustomer
@@ -43,9 +43,7 @@ CREATE TABLE `tblcustomer`  (
 INSERT INTO `tblcustomer` VALUES (1, 'Admin', '08321555', '002341', 'KPC', 'iphone15.jpg', '15 Pro max', 1500, 1.3, 8, '2023-05-15 14:23:46', 23);
 INSERT INTO `tblcustomer` VALUES (2, 'Sonita', '092333124', '002414', 'PP', 'Mac.jpg', 'MAC Book', 2300, 0.8, 12, '2023-08-06 14:33:36', 43);
 INSERT INTO `tblcustomer` VALUES (3, 'G-Devit', '012222222', '002 413', 'PP', 'Ipad.jpg', 'Ipad', 1290, NULL, NULL, NULL, NULL);
-INSERT INTO `tblcustomer` VALUES (20231212051222, 'Twin_Gabby', '085 808 587', '5555', 'PP', 'photo_2023-05-18_12-21-17.jpg', 'Huawei', 2500, 1.5, 10, '2023-12-12 05:12:22', 6);
-INSERT INTO `tblcustomer` VALUES (20231213031250, 'Be Chkout', '5656', '123', 'KPC', '32.jpeg', 'Vivo', 2500, 0.5, 12, '2023-12-13 03:12:50', 10);
-INSERT INTO `tblcustomer` VALUES (20231226111212, 'Regret', '577583', '123', 'VN', '24.jpeg', 'Huawei', 333, 0.5, 10, '2023-12-26 11:12:12', 1);
+INSERT INTO `tblcustomer` VALUES (20260622180641, 'fdsfds', 'safdsafsa', 'sfdfsaf', 'asdsf', 'Rog.png', 'rog', 100, 10, 2, '2026-06-22 18:06:41', 65464);
 
 -- ----------------------------
 -- Table structure for tbldepreciation
@@ -60,17 +58,17 @@ CREATE TABLE `tbldepreciation`  (
   `Clear_Date` datetime NULL DEFAULT NULL,
   `clear_by_userid` bigint NULL DEFAULT NULL,
   PRIMARY KEY (`Depreid`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 20231212051276 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 20231212051290 CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of tbldepreciation
 -- ----------------------------
-INSERT INTO `tbldepreciation` VALUES (1, 1, 200, 40, '2023-07-17 10:35:23', '0000-00-00 00:00:00', 1);
-INSERT INTO `tbldepreciation` VALUES (2, 1, 200, 40, '2023-08-17 20:27:26', '0000-00-00 00:00:00', 1);
+INSERT INTO `tbldepreciation` VALUES (1, 1, 200, 40, '2023-07-17 10:35:23', '2025-01-01 00:00:00', 1);
+INSERT INTO `tbldepreciation` VALUES (2, 1, 200, 40, '2023-08-17 20:27:26', '2025-01-01 00:00:00', 1);
 INSERT INTO `tbldepreciation` VALUES (3, 1, 200, 40, '2023-09-17 10:41:03', '2023-12-19 10:26:00', -2);
-INSERT INTO `tbldepreciation` VALUES (4, 2, 240, 40, '2023-07-21 10:35:23', NULL, NULL);
-INSERT INTO `tbldepreciation` VALUES (5, 2, 240, 40, '2023-08-21 10:42:30', NULL, NULL);
-INSERT INTO `tbldepreciation` VALUES (6, 2, 240, 40, '2023-09-21 10:42:48', NULL, NULL);
+INSERT INTO `tbldepreciation` VALUES (4, 2, 240, 40, '2023-07-21 10:35:23', '2026-05-10 17:48:00', -2);
+INSERT INTO `tbldepreciation` VALUES (5, 2, 240, 40, '2023-08-21 10:42:30', '2026-05-10 17:55:00', -2);
+INSERT INTO `tbldepreciation` VALUES (6, 2, 240, 40, '2023-09-21 10:42:48', '2026-05-10 17:55:00', -2);
 INSERT INTO `tbldepreciation` VALUES (7, 3, 258, 38.7, '2023-11-21 10:50:15', '2023-11-22 09:21:54', 3);
 INSERT INTO `tbldepreciation` VALUES (8, 3, 258, 38.7, '2023-12-21 10:50:15', '2023-12-22 09:22:01', 3);
 INSERT INTO `tbldepreciation` VALUES (9, 3, 258, 38.7, '2024-01-21 10:50:15', '2024-01-22 09:22:11', 3);
@@ -130,6 +128,20 @@ INSERT INTO `tbldepreciation` VALUES (20231212051272, 20231227091252, 10.0909090
 INSERT INTO `tbldepreciation` VALUES (20231212051273, 20231227091252, 10.090909090909, 12.21, '2024-09-27 00:00:00', NULL, NULL);
 INSERT INTO `tbldepreciation` VALUES (20231212051274, 20231227091252, 10.090909090909, 12.21, '2024-10-27 00:00:00', NULL, NULL);
 INSERT INTO `tbldepreciation` VALUES (20231212051275, 20231227091252, 10.090909090909, 12.21, '2024-11-27 00:00:00', NULL, NULL);
+INSERT INTO `tbldepreciation` VALUES (20231212051276, 20260622180645, 8.3333333333333, 1.2, '2026-07-22 00:00:00', '2026-06-22 18:44:00', -2);
+INSERT INTO `tbldepreciation` VALUES (20231212051277, 20260622180645, 8.3333333333333, 1.2, '2026-08-22 00:00:00', '2026-06-22 18:44:00', -2);
+INSERT INTO `tbldepreciation` VALUES (20231212051278, 20260622180645, 8.3333333333333, 1.2, '2026-09-22 00:00:00', NULL, NULL);
+INSERT INTO `tbldepreciation` VALUES (20231212051279, 20260622180645, 8.3333333333333, 1.2, '2026-10-22 00:00:00', NULL, NULL);
+INSERT INTO `tbldepreciation` VALUES (20231212051280, 20260622180645, 8.3333333333333, 1.2, '2026-11-22 00:00:00', NULL, NULL);
+INSERT INTO `tbldepreciation` VALUES (20231212051281, 20260622180645, 8.3333333333333, 1.2, '2026-12-22 00:00:00', NULL, NULL);
+INSERT INTO `tbldepreciation` VALUES (20231212051282, 20260622180645, 8.3333333333333, 1.2, '2027-01-22 00:00:00', NULL, NULL);
+INSERT INTO `tbldepreciation` VALUES (20231212051283, 20260622180645, 8.3333333333333, 1.2, '2027-02-22 00:00:00', NULL, NULL);
+INSERT INTO `tbldepreciation` VALUES (20231212051284, 20260622180645, 8.3333333333333, 1.2, '2027-03-22 00:00:00', NULL, NULL);
+INSERT INTO `tbldepreciation` VALUES (20231212051285, 20260622180645, 8.3333333333333, 1.2, '2027-04-22 00:00:00', NULL, NULL);
+INSERT INTO `tbldepreciation` VALUES (20231212051286, 20260622180645, 8.3333333333333, 1.2, '2027-05-22 00:00:00', NULL, NULL);
+INSERT INTO `tbldepreciation` VALUES (20231212051287, 20260622180645, 8.3333333333333, 1.2, '2027-06-22 00:00:00', NULL, NULL);
+INSERT INTO `tbldepreciation` VALUES (20231212051288, 20260622180641, 50, 10, '2026-07-22 00:00:00', '2026-06-22 18:48:00', -2);
+INSERT INTO `tbldepreciation` VALUES (20231212051289, 20260622180641, 50, 10, '2026-08-22 00:00:00', '2026-06-22 18:48:00', -2);
 
 -- ----------------------------
 -- Table structure for tblusername
@@ -137,41 +149,22 @@ INSERT INTO `tbldepreciation` VALUES (20231212051275, 20231227091252, 10.0909090
 DROP TABLE IF EXISTS `tblusername`;
 CREATE TABLE `tblusername`  (
   `userid` bigint NOT NULL,
-  `username` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
-  `userpassword` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
-  `Photo` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
+  `username` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL,
+  `userpassword` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL,
+  `Photo` text CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NULL,
   PRIMARY KEY (`userid`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB CHARACTER SET = utf8mb3 COLLATE = utf8mb3_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of tblusername
 -- ----------------------------
-INSERT INTO `tblusername` VALUES (100, 'K\'jen', '999', 'excel/7.jpeg');
 INSERT INTO `tblusername` VALUES (101, 'Admin', '123', 'excel/17.jpeg');
 INSERT INTO `tblusername` VALUES (102, 'Thea', '001', 'excel/13.jpeg');
-INSERT INTO `tblusername` VALUES (20231205051214, 'Levimis', '55555', '34.jpeg');
-INSERT INTO `tblusername` VALUES (20231206031226, 'Ah jm', '55555', '4.jpeg');
 
 -- ----------------------------
 -- View structure for view_customer_paid
 -- ----------------------------
 DROP VIEW IF EXISTS `view_customer_paid`;
-CREATE ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `view_customer_paid` AS SELECT
-	tblcustomer.cusid, 
-	tblcustomer.cusname, 
-	tblcustomer.custel, 
-	tblcustomer.cusAddress, 
-	tblcustomer.Photo, 
-	tbldepreciation.Principal, 
-	tbldepreciation.Interest_Month, 
-	tbldepreciation.Paid_Date, 
-	tbldepreciation.Clear_Date, 
-	tbldepreciation.clear_by_userid
-FROM
-	tblcustomer
-	INNER JOIN
-	tbldepreciation
-	ON 
-		tblcustomer.cusid = tbldepreciation.cusid ;
+CREATE ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `view_customer_paid` AS select `tblcustomer`.`cusid` AS `cusid`,`tblcustomer`.`cusname` AS `cusname`,`tblcustomer`.`custel` AS `custel`,`tblcustomer`.`cusAddress` AS `cusAddress`,`tblcustomer`.`Photo` AS `Photo`,`tbldepreciation`.`Principal` AS `Principal`,`tbldepreciation`.`Interest_Month` AS `Interest_Month`,`tbldepreciation`.`Paid_Date` AS `Paid_Date`,`tbldepreciation`.`Clear_Date` AS `Clear_Date`,`tbldepreciation`.`clear_by_userid` AS `clear_by_userid` from (`tblcustomer` join `tbldepreciation` on((`tblcustomer`.`cusid` = `tbldepreciation`.`cusid`)));
 
 SET FOREIGN_KEY_CHECKS = 1;
